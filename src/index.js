6ac1b1c1cab00af6e1f195e47b70b864e97dbf28
khristoph <christopherfriedman@gmail.com>
2017-12-10 13:19:23 -0600

    Allows the searching of youtubes API for videos

--- src/index.js
+++ src/index.js
@@ -16,21 +16,34 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: YT_API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term){
+    YTSearch({key: YT_API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
       // => is shorthand for 'function'
       //key and value are both videos { videos: videos}
     });
+
   }
 
   render() {
     return (
      <div>
-       <SearchBar />
-       <VideoDetail video={this.state.videos[0]} />
-       <VideoList videos={this.state.videos} />
+       <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+       <VideoDetail video={this.state.selectedVideo} />
+       <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        videos={this.state.videos} />
      </div>
     );
   }
